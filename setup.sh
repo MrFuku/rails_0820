7a80e0053f49ea5deca7ea4e8ddabece3ea708d3
MrFuku <mmafighter_shun@icloud.com>
2021-08-09 11:35:50 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# apt（Macでいうbrew）の更新
+# gitで使うエディタを指定
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+
+# apt（Macで言うところのbrew）の更新
 sudo apt update -y
-sudo apt upgrade -y
 
 # 環境構築に必要なパッケージのインストール
 sudo apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev
 
 # rbenvのセットアップ
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH=$HOME/.rbenv/bin:$PATH' >> ~/.bashrc
+export PATH=$HOME/.rbenv/bin:$PATH
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+eval "$(rbenv init -)"
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-source ~/.bashrc
 
 # rubyのインストール
 rbenv install 2.7.2
@@ -20,9 +23,10 @@ rbenv global 2.7.2
 
 # nodeのセットアップ
 git clone https://github.com/nodenv/nodenv.git ~/.nodenv
-echo 'export PATH="$HOME/.nodenv/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH=$HOME/.nodenv/bin:$PATH' >> ~/.bashrc
+export PATH=$HOME/.nodenv/bin:$PATH
 echo 'eval "$(nodenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+eval "$(nodenv init -)"
 git clone https://github.com/nodenv/node-build.git $(nodenv root)/plugins/node-build
 
 # nodeのインストール
@@ -31,5 +35,7 @@ nodenv global 12.14.0
 
 # railsのセットアップ
 bundle
+rbenv rehash
+rails db:create
 rails db:migrate
 rails db:seed
