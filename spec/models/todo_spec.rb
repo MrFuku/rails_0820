14fe0702b8d27067fcba48f87419e93c27e505cc
MrFuku <mmafighter_shun@icloud.com>
2021-08-31 15:07:49 +0900

    Change to MySQL in ci

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
   it 'Todoが作れること' do
-    todo = Todo.new(content: 'Hello')
+    todo = Todo.create(content: 'Hello')
     expect(todo).to be_valid
   end
 end
